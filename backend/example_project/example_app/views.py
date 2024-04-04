81a8f087406c705728ca90d681f54301999b58b7
edivis0325@gmail.com <edivis0325@gmail.com>
2024-04-04 18:36:55 +0900

    Chapter2 초기설정

---
+++ backend/example_project/example_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello, World!")
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def hello_rest_api(request):
+    data = {'message': 'Hello, REST API!'}
+    return Response(data)
